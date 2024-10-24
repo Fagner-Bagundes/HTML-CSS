15cd34a09e004e271e8ae30c399d872929e16ddc
Fagner_0 <ferreirabagundes@gmail.com>
2024-10-24 18:14:39 -0300

    iniciando os códigos com js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-const desafios = document.querySelector('.desafios-cards-f');
-const desafiodo = document.querySelectorAll('.desafio-cards-f div');
+const slider = document.querySelectorAll('.desafio-f');
+const btnVoltar = document.querySelector('.btn-voltar');
+const btnAvancar = document.querySelector('.btn-avancar');
 
-console.log(desafios)
+let currentSlide = 0;
+
+function hideSlider() {
+    slider.forEach(item = item.classList.remove('on'))
+};
+
+function showSlider() {
+    slider[currentSlide].classList.add('on')
+}
+
+function avancarSlide(){
+    
+}
+
+btnAvancar.addEventListener(`click`, () => console.log(`Clicado`))
+
+btnVoltar.addEventListener(`click`, () => console.log(`Clicado`))
