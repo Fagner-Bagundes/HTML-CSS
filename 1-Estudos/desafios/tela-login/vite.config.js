759d05a1421ccfc5ca501d15aa74d8c2981b8a62
FagnerFerreira <ferreirabagundes@gmail.com>
2025-01-24 18:07:26 -0300

    iniciando projeto

--- 1-Estudos/desafios/tela-login/vite.config.js
+++ 1-Estudos/desafios/tela-login/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    root: '/src',
+    root: './src',
     base: '/',
     server: {
         port: 3000,
     },
     build: {
-        outDir: '../dist'
+        outDir: '../'
     }
 })
